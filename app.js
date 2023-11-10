3ac052e05f88c1ac01d5bd06d8d05cb8aa1dd602
Ahsankhan556 <ak5589667@gmail.com>
2023-11-11 04:47:48 +1030

    javascript chapter43to48

---
+++ app.js
@@ -0,0 +1,61 @@
+function callAlert(){
+    alert("Thanks For Click")
+}
+function phone11(){
+alert(
+   " Apple iPhone 11 Pro Pre-Owned Unlocked (64GB) - Gray Price: $439.44 "
+)
+
+}
+function phone12(){
+    alert(
+       " OtterBox Apple iPhone 12/iPhone 12 Pro Symmetry with MagSafe Case – Black Price: $35.44 "
+    )
+    }    
+
+    function phone13(){
+alert(
+    "Boost Mobile Prepaid Apple iPhone SE 5G (3rd Generation) (64GB) - Black Price: $349.45"
+)
+
+    }
+
+
+function phone14(){
+alert(
+    "Apple iPhone 14 Pro Price: $500.46"
+
+)
+
+
+
+}
+function phone15(){
+
+alert(
+    "Apple iPhone 15 Pro Price: $750.46"
+)
+
+}
+// function dela(){
+
+    
+//     document.getElementById("ahsan").innerHTML=""
+//     document.getElementById("ahsa").innerHTML=""
+// }
+
+function delete1(){
+
+document.getElementById("ahsan").innerHTML=""
+
+}
+function delete2(){
+    document.getElementById("bilal").innerHTML=""
+}
+function delete3(){
+    document.getElementById("umar").innerHTML=""
+}
+function delete4(){
+    document.getElementById("nadim").innerHTML=""
+}
+
